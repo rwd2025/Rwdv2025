da181d5d826128b863aa14020c93724ec4af8909
rwd2025 <jameswsjacobs87@gmail.com>
2026-05-30 06:45:10 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE="rwd-001-home-route-lock";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./style.css","./app.js","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
